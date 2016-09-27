46c1bda8bf88bae8eea3ffb9cdfcfd310bc7f035
unknown <>
2016-09-27 19:59:45 +0800

    fix phonenumber

--- js/component/keroa-phoneNumber.js
+++ js/component/keroa-phoneNumber.js
@@ -5,27 +5,24 @@
  */
 import {IntegerAdapter} from './keroa-integer';
 import {NumberFormater} from 'neoui-sparrow/js/util/formater';
-import {PercentMasker} from 'neoui-sparrow/js/util/masker';
+import {PhoneNumberMasker} from 'neoui-sparrow/js/util/masker';
 import {core} from 'neoui-sparrow/js/core';
 import {compMgr} from 'neoui-sparrow/js/compMgr';
 /**
- * 百分比控件
+ * 手机号控件
  */
 var PhoneNumberAdapter = IntegerAdapter.extend({
     init: function () {
         PhoneNumberAdapter.superclass.init.apply(this);
         this.validType = 'phoneNumber';
         this.maskerMeta = core.getMaskerMeta('phoneNumber') || {};
-        this.maskerMeta.precision = this.getOption('phoneNumber') || this.maskerMeta.phoneNumber;
-        if (this.maskerMeta.precision){
-            this.maskerMeta.precision = parseInt(this.maskerMeta.precision) + 2;
-        }
+        this.maskerMeta.precision = this.getOption('precision') || this.maskerMeta.precision;
         this.formater = new NumberFormater(this.maskerMeta.precision);
-        this.masker = new PercentMasker(this.maskerMeta);
+        this.masker = new PhoneNumberMasker(this.maskerMeta);
     }
 });
 compMgr.addDataAdapter({
-	adapter: PercentAdapter,
-	name: 'percent'
+	adapter: PhoneNumberAdapter,
+	name: 'phoneNumber'
 });
-export {PercentAdapter};
+export {PhoneNumberAdapter};
