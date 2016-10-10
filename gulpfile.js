196707f5a8589a11d708fd7ca4536b159dd4e40b
onvno <leewei2020@126.com>
2016-10-10 15:20:15 +0800

    更改tinper-neoui依赖

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ var rename = require('gulp-rename');
  * 需求：第三方包打进来，之后需要优化
  */
 gulp.task('libui', function() {
-	return gulp.src(['./dist/js/u.js','./node_modules/neoui/vendor/ui/*.js'])
+	return gulp.src(['./dist/js/u.js','./node_modules/tinper-neoui/vendor/ui/*.js'])
 		.pipe(concat('u.js'))
 		.pipe(gulp.dest('./dist/js'))
 		.pipe(uglify())
@@ -16,13 +16,13 @@ gulp.task('libui', function() {
 });
 
 gulp.task('dist', ['libui'], function(){
-	gulp.src(['./node_modules/neoui/dist/css/**','./node_modules/neoui-grid/dist/css/**','./node_modules/neoui-tree/dist/css/**'])
+	gulp.src(['./node_modules/tinper-neoui/dist/css/**','./node_modules/neoui-grid/dist/css/**','./node_modules/neoui-tree/dist/css/**'])
 		.pipe(gulp.dest('./dist/css'));
 
-	gulp.src('./node_modules/neoui/dist/fonts/**')
+	gulp.src('./node_modules/tinper-neoui/dist/fonts/**')
 		.pipe(gulp.dest('dist/fonts'));
 
-	gulp.src('./node_modules/neoui/dist/images/**')
+	gulp.src('./node_modules/tinper-neoui/dist/images/**')
 		.pipe(gulp.dest('./dist/images'));
 
 	gulp.src(['./node_modules/neoui-grid/dist/js/**', './node_modules/neoui-tree/dist/js/**','./node_modules/tinper-neoui-polyfill/dist/**'])
