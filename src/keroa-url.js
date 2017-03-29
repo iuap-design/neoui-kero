b07d09ed84f06fb5d071925afcf646703fc31431
LiuYueKai <liuyuekainihao@163.com>
2017-03-29 14:10:44 +0800

    url组件支持默认校验规则

--- src/keroa-url.js
+++ src/keroa-url.js
@@ -15,8 +15,9 @@ import {
 
 var UrlAdapter = StringAdapter.extend({
     init: function() {
-        this.validType = 'url';
+
         UrlAdapter.superclass.init.apply(this);
+        this.validType = 'url';
         /*
          * 因为需要输入，因此不显示为超链接
          */
