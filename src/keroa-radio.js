95d7ad6968f6ecf871572eac5f5684b19231b2b5
huyueb <1062887235@qq.com>
2017-06-28 15:25:29 +0800

    解决value为null时bug

--- src/keroa-radio.js
+++ src/keroa-radio.js
@@ -207,7 +207,7 @@ var RadioAdapter = u.BaseAdapter.extend({
                     if (comp) {
                         var inptuValue = comp._btnElement.value;
                         //解决boolean类型的true和false与"true"和"false"比较
-                        if (inptuValue && (inptuValue == value || inptuValue == value.toString())) {
+                        if (inptuValue && inptuValue == value.toString) {
                             fetch = true;
                             addClass(comp.element, 'is-checked')
                             comp._btnElement.click();
